ab8285b27d5382fd486d1243aab52bbf9b2ebf76
Vihaan Aneja <vihaananejaa@gmail.com>
2026-09-03 16:48:20 +0000

    commit1

---
+++ The syntax/statements.c
@@ -0,0 +1,16 @@
+// every statement in C must end with a semicolon (;), otherwise it will result in a syntax error. 
+//The semicolon is used to separate statements and indicate the end of a statement. It is important to remember to include a semicolon at the end of each statement to avoid compilation errors.
+
+#include <stdio.h>
+int main() {
+    printf("Hello World!");
+}
+
+// Many statements
+#include <stdio.h>
+int main() {
+printf("Hello World!");
+printf("Have a good day!");
+return 0;
+}
+
---
+++ The syntax/syntax.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+
+// The line 1 is the header file that contains the declaration of the printf function. The line 3 is the main function that is the entry point of the program. The line 4 is the printf function that prints the string "Hello World!" to the console. The line 5 is the return statement that returns 0 to indicate that the program has completed successfully.
